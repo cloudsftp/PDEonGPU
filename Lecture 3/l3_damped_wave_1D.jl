2f591f677432561a3789ac089588f6b749a8e1cb
Fabian Weik <fabian.weik@protonmail.com>
2025-02-01 09:22:51 +0100

    finish lecture 5

---
+++ Lecture 3/l3_damped_wave_1D.jl
@@ -0,0 +1,39 @@
+using Plots, Plots.Measures, Printf
+default(size=(1200, 400), framestyle=:box, label=false, grid=false, margin=10mm, lw=6, labelfontsize=20, tickfontsize=20, titlefontsize=24)
+
+@views function diffusion_1D()
+    # physics
+    lx   = 20.0
+    dc   = 1.0
+    ρ   = 20.0
+
+    # numerics
+    nx   = 200
+    nvis = 50
+
+    # derived numerics
+    dx   = lx / nx
+    dt   = dx^2 / dc / 2
+    nt   = nx^2 ÷ 5
+    xc   = LinRange(dx / 2, lx - dx / 2, nx)
+
+    # array initialisation
+    C    = @. 1.0 + exp(-(xc-lx/4)^2) - xc/lx
+    C_i  = copy(C)
+    qx   = zeros(Float64, nx - 1)
+
+    # time loop
+    for it = 1:nt
+        #qx          .-= dt / (ρ * dc + dt) .* (qx + dc .* diff(C) ./ dx)
+        qx          .-= (dt / dc .* qx + diff(C) ./ dx) ./ ρ
+        C[2:end-1]  .-= dt .* diff(qx) ./ dx
+
+        if it % nvis == 0
+            display(plot(xc, [C_i, C]; xlims=(0, lx), ylims=(-0.1, 2.1),
+                         xlabel="lx", ylabel="Concentration",
+                         title="time = $(round(it*dt,digits=1))"))
+        end
+    end
+end
+
+diffusion_1D()
---
+++ Lecture 5/Pf_diffusion_2D_perf_loop_fun.jl
@@ -0,0 +1,117 @@
+using Plots, Plots.Measures, Printf
+using BenchmarkTools
+default(size=(600, 500), framestyle=:box, label=false, grid=false, margin=10mm, lw=6, labelfontsize=11, tickfontsize=11, titlefontsize=11)
+
+macro d_xa(A)  esc(:( $A[x+1,y]-$A[x,y] )) end
+macro d_ya(A)  esc(:( $A[x,y+1]-$A[x,y] )) end
+
+function compute_flux!(qDx, qDy, Pf, k_ηf_dx, k_ηf_dy, _1_θ_dτ)
+    nx, ny = size(Pf)
+    Threads.@threads for y in 1:ny
+        for x in 1:nx-1
+            qDx[x+1,y] -= (qDx[x+1,y] + k_ηf_dx * @d_xa(Pf)) * _1_θ_dτ
+        end
+    end
+
+    Threads.@threads for y in 1:ny-1
+        for x in 1:nx
+            qDy[x,y+1] -= (qDy[x,y+1] + k_ηf_dy * @d_ya(Pf)) * _1_θ_dτ
+        end
+    end
+
+    return nothing
+end
+
+function update_Pf!(Pf,qDx,qDy,_dx,_dy,_β_dτ)
+    nx, ny = size(Pf)
+    Threads.@threads for y in 1:ny
+        for x in 1:nx
+            Pf[x,y] -= _β_dτ * (_dx * @d_xa(qDx) + _dy * @d_ya(qDy))
+        end
+    end
+
+    return nothing
+end
+
+function compute!(Pf, qDx, qDy, k_ηf_dx, k_ηf_dy, _1_θ_dτ, _dx, _dy, _β_dτ)
+    compute_flux!(qDx, qDy, Pf, k_ηf_dx, k_ηf_dy, _1_θ_dτ)
+    update_Pf!(Pf, qDx, qDy, _dx, _dy, _β_dτ)
+    return nothing
+end
+
+function Pf_diffusion_2D(; do_check=false)
+    # physics
+    lx, ly = 20.0, 20.0
+    k_ηf   = 1.0
+
+    # numerics
+    nx, ny  = 500, 500
+    ϵtol    = 1e-8
+    maxiter = 20max(nx, ny)
+    ncheck  = ceil(Int, 0.25max(nx, ny))
+    cfl     = 1.0 / sqrt(2.1)
+    re      = 2π
+
+    # derived numerics
+    dx, dy  = lx / nx, ly / ny
+    xc, yc  = LinRange(dx / 2, lx - dx / 2, nx), LinRange(dy / 2, ly - dy / 2, ny)
+    θ_dτ    = max(lx, ly) / re / cfl / min(dx, dy)
+    β_dτ    = (re * k_ηf) / (cfl * min(dx, dy) * max(lx, ly))
+
+    # array initialisation
+    Pf      = @. exp(-(xc - lx / 2)^2 - (yc' - ly / 2)^2)
+    qDx     = zeros(Float64, nx + 1, ny)
+    qDy     = zeros(Float64, nx, ny + 1)
+    r_Pf    = zeros(nx, ny)
+
+    # precompute
+    k_ηf_dx = k_ηf / dx
+    k_ηf_dy = k_ηf / dy
+
+    _1_θ_dτ = 1.0 / (1.0 + θ_dτ)
+    _β_dτ = 1.0 / β_dτ
+
+    _dx = 1.0 / dx
+    _dy = 1.0 / dy
+    # end precompute
+
+    tic = time()
+    niter = 0
+    warmup = 11
+
+    # iteration loop
+    iter = 1; err_Pf = 2ϵtol
+    while err_Pf >= ϵtol && iter <= maxiter
+        compute!(Pf, qDx, qDy, k_ηf_dx, k_ηf_dy, _1_θ_dτ, _dx, _dy, _β_dτ)
+
+        if do_check && iter % ncheck == 0
+            r_Pf .= diff(qDx, dims=1) ./ dx .+ diff(qDy, dims=2) ./ dy
+            err_Pf = maximum(abs.(r_Pf))
+
+            @printf("  iter/nx=%.1f, err_Pf=%1.3e\n", iter / nx, err_Pf)
+
+            display(heatmap(xc, yc, Pf'; xlims=(xc[1], xc[end]), ylims=(yc[1], yc[end]), aspect_ratio=1, c=:turbo, clim=(0, 1)))
+        end
+
+        if iter == warmup
+            tic = time()
+            niter = 0
+        end
+
+        iter += 1
+        niter += 1
+    end
+
+    toc = time() - tic
+    tit = toc / niter
+    A_eff = (3*2)/1e9*nx*ny*sizeof(Float64)
+    T_eff = A_eff/tit
+
+    @printf("Time = %1.3f sec, T_eff = %1.2f GB/s (niter = %d)\n", toc, round(T_eff, sigdigits=3), niter)
+
+    return
+end
+
+@show Threads.nthreads()
+
+Pf_diffusion_2D(; do_check=false)
